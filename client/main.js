9f46ce028c27188d247fd365baf7163131e6e2b7
Alejandro Galeano <ale.gb32@gmail.com>
2020-06-28 00:50:49 -0300

    Establecida la coneccion entre los participantes, envio y recepcion de mensajes, estilizado de la caja de chat

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ var socket = io.connect('http://192.168.0.41:6677',{'forceNew':true});
      render(data);
     });
  function render(datos){
+    // cargar los mjes en la caja del chat
     let html = datos.map((message,index)=>{
         return(`
             <div class="message">
@@ -13,5 +14,24 @@ var socket = io.connect('http://192.168.0.41:6677',{'forceNew':true});
         `);
     }).join(' ');
 
-    document.getElementById('messages').innerHTML = html;
+    let msgs = document.getElementById('messages');
+    msgs.innerHTML = html;
+
+    // visualizar mensajes mas recientes
+    msgs.scrollTop = msgs.scrollHeight;
+ };
+
+ //capturar los mjes del formulario
+ function addMessage(e){
+    // parsear los valores
+    var message = {
+        nickname : document.getElementById('nickname').value,
+        text : document.getElementById('text').value
+    };
+    //ocultar el nickname para que se mantenga la conversacion con el mismo
+    document.getElementById('nickname').style.display = 'none';
+    //enviar el mje al servidor
+    socket.emit('add-message', message);
+
+    return false;
  };
--- server/index.js
+++ server/index.js
@@ -1,24 +1,37 @@
+// crear el server
 var express = require("express");
 var app = express();
-
 var server = require("http").Server(app);
-
 var io = require('socket.io')(server);
 
-app.use(express.static('client'))
-app.get("/",(req,res)=>res.status(200).send("Hola mundo"));
+//indicar los archivos que se ultilizaran para la vista
+app.use(express.static('client'));
 
+//app.get("/",(req,res)=>res.status(200).send("Hola mundo"));
+
+// crear el array donde se almacenan los mjes mientras corra el servicio
 let messages = [{
     id:1,
     text: "Bienvenido al chat",
     nickname: "Bot"
 }]
 
+//logica de comunicacion, se detecta los dispositivos, se envian los archivos actuales
+//y se reciben los nuevos
 io.on('connection',(socket)=>{
+    //informar el dispositivo
     console.log("El nodo: "+ socket.handshake.address +" se ha conectado...");
+    //enviar los mjes existentes
     socket.emit('messages',messages);
+    // recibir los nuevos mjes
+    socket.on('add-message',(data)=>{
+        //almacenar en el array los nuevos mjes
+        messages.push(data);
+        //enviar al resto de los participantes los nuevos mjes
+        io.sockets.emit('messages', messages);
+    });
 });
 
 
-
+// escucha activa del servidor
 server.listen(6677,()=>console.log("servidor corriendo en localhost:6677"));
